bbdf0fd972814db96dfb41ff80d578fade86a71c
Jean Jordaan <jean.jordaan@gmail.com>
2013-05-12 18:09:40 +0700

    Back out mailin from here, moved to mailin.py

--- hl/plone/boardnotifications/browser/views.py
+++ hl/plone/boardnotifications/browser/views.py
@@ -4,9 +4,6 @@ from Products import Five
 from Products.CMFCore.utils import getToolByName
 from plone.app.layout.viewlets.common import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-import re
-import email
-import urlparse
 
 class Subscribe(Five.BrowserView):
 
@@ -45,41 +42,3 @@ class SubscriptionViewlet(ViewletBase):
         subscriptions = getUtility(ISubscriptions)
         return subscriptions.check_subscriber(self.context)
 
-class MailIn(Five.BrowserView):
-
-    def __call__(self):
-        mailstr = self.request.get('Mail')
-        if not mailstr:
-            return
-        mtool = getToolByName(self.context, 'portal_membership')
-        msg = email.message_from_string(mailstr)
-        msg_from = msg['From']
-        member = mtool.searchMembers('email', msg_from)
-        if not member:
-            return
-        body = msg.get_payload(decode=True)
-        if not body:
-            # e.g. multipart ...
-            return
-        # grabbed from http://stackoverflow.com/questions/4696418/regex-to-extract-all-urls-from-a-page
-        subj = msg['Subject']
-        url_re = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
-        target = None
-        for match in url_re.finditer(body):
-            url = match.group(0)
-            path = urlparse.urlparse(url).path
-            obj = self.context.restrictedTraverse(path)
-            if obj.meta_type in ['PloneboardComment',
-                    'PloneboardConversation']:
-                if target is None:
-                    target = obj
-                elif (obj.meta_type == 'PloneboardComment' and
-                        target.meta_type == 'PloneboardConversation'):
-                    # More specific wins
-                    target = obj
-            if not target:
-                return
-            if target.meta_type == 'PloneboardConversation':
-                target.addComment('answer via mail'+subj, body, creator=member)
-            elif target.meta_type == 'PloneboardComment':
-                target.addReply('answer via mail'+subj, body, creator=member)
